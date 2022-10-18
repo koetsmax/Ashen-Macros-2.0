e50d99d8abf6e5dce1178846337c7c33ebc809ca
koetsmax <koetsmax@gmail.com>
2022-10-18 16:10:53 +0200

    SOT Official module built

--- Modules/staffcheck.py
+++ Modules/staffcheck.py
@@ -245,11 +245,12 @@ class StaffCheck:
         print(self.currentstate)
         self.functionbutton.config(text="Cool Button", command=None)
         self.killbutton.config(text="Kill Program", command=self.kill)
-        self.startbutton.config(text="Start Check!", command=self.continuetonext)
+        self.startbutton.config(text="Start Check!", command=self.startcheck)
         if self.method.get() != "All Commands":
             _UpdateStatus(self, "Check Completed!!!", 100)
             self.functionbutton.state(['disabled'])
             self.startbutton.state(['!disabled'])
+            self.log.see("end")
             try:
                 self.save_button.state(['!disabled'])
             except:
--- Modules/submodules/SOTOfficial.py
+++ Modules/submodules/SOTOfficial.py
@@ -1,5 +1,34 @@
 import keyboard
-
+from submodules.functions.ClearTypingBar import _ClearTypingBar
+from submodules.functions.SwitchChannel import _SwitchChannel
+from submodules.functions.UpdateStatus import _UpdateStatus
 
 def _SOTOfficial(self):
-    print(self.method.get())
+    self.currentstate = "SOTOfficial"
+    _SwitchChannel(self, "#official-swag")
+    _ClearTypingBar(self)
+    _UpdateStatus(self, "Status: Searching through Sea of Thieves official", 50)
+    keyboard.press_and_release('ctrl+f')
+    keyboard.press_and_release("ctrl+a")
+    keyboard.press_and_release("backspace")
+    keyboard.write(f'from: {self.userID.get()}')
+    keyboard.press_and_release('enter')
+    _UpdateStatus(self, "Status: Done searching through Sea of Thieves official", 55)
+
+    self.functionbutton.config(text="Narrow Search Results", command=lambda: NarrowResults(self))
+    self.startbutton.config(text="Continue", command=self.continuetonext)
+    self.startbutton.state(['!disabled'])
+    self.functionbutton.state(['!disabled'])
+    _UpdateStatus(self, "Press ONE of the buttons to do what you want to do", "")
+
+def NarrowResults(self):
+    self.functionbutton.state(['disabled'])
+    self.startbutton.state(['disabled'])
+    _ClearTypingBar(self)
+    keyboard.press_and_release('ctrl+f')
+    keyboard.press_and_release("ctrl+a")
+    keyboard.press_and_release("backspace")
+    keyboard.write(f'from: {self.userID.get()} alliance')
+    keyboard.press_and_release('enter')
+    _UpdateStatus(self, "Narrowed search results!", "")
+    self.startbutton.state(['!disabled'])
