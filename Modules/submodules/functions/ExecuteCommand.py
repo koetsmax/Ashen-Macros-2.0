be6e100937295be28b5054427dab6a5c807ae159
koetsmax <koetsmax@gmail.com>
2022-10-21 19:13:53 +0200

    Increased delay on initial command to stop discord from being a little shit

--- Modules/submodules/functions/ExecuteCommand.py
+++ Modules/submodules/functions/ExecuteCommand.py
@@ -5,8 +5,8 @@ from submodules.functions.UpdateStatus import _UpdateStatus
 def _ExecuteCommand(self, command, subcommands):
     _UpdateStatus(self, f"Status: Executing command: {command} with parameters: {subcommands}", "")
     keyboard.write(command)
-    time.sleep(1.6)
-    _UpdateStatus(self, "Status: sleeping for 1.6 seconds so Discord can catch up", "")
+    time.sleep(2)
+    _UpdateStatus(self, "Status: sleeping for 2 seconds so Discord can catch up", "")
     keyboard.press_and_release('tab')
     for subcommand in subcommands:
         keyboard.write(subcommand)
