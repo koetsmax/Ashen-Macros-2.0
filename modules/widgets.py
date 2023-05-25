9013e74165a2881656910e65530b7d431e611d1c
koetsmax <koetsmax@gmail.com>
2023-05-25 20:30:36 +0200

    Added create_checkbox function to widgets module

--- modules/widgets.py
+++ modules/widgets.py
@@ -1,52 +1,77 @@
-from tkinter import ttk as tk
-from tkinter import StringVar
+from tkinter import ttk
+import tkinter as tk
 from typing import List, Callable
+import configparser
 
 
 def create_button(
-    parent: tk.Widget,
+    parent: ttk.Widget,
     text: str,
     command: Callable,
     row: int,
     column: int,
     sticky: str = "",
-) -> tk.Button:
-    button = tk.Button(parent, text=text, command=command)
+) -> ttk.Button:
+    button = ttk.Button(parent, text=text, command=command)
     button.grid(row=row, column=column, sticky=sticky)
     return button
 
 
 def create_label(
-    parent: tk.Widget, text: str, row: int, column: int, sticky: str = ""
-) -> tk.Label:
-    label = tk.Label(parent, text=text)
-    label.grid(row=row, column=column, sticky=sticky)
+    parent: ttk.Widget,
+    text: str,
+    row: int,
+    column: int,
+    sticky: str = "",
+    rowspan: int = 1,
+    columnspan: int = 1,
+) -> ttk.Label:
+    """
+    Creates a label widget and places it in the parent widget.
+    """
+    label = ttk.Label(parent, text=text)
+    label.grid(
+        row=row, column=column, sticky=sticky, rowspan=rowspan, columnspan=columnspan
+    )
     return label
 
 
 def create_listbox(
-    parent: tk.Widget,
+    parent: ttk.Widget,
     items: List[str],
-    variable: StringVar,
+    variable: tk.StringVar,
     row: int,
     column: int,
     sticky: str = "",
-) -> tk.Combobox:
-    combobox = tk.Combobox(parent, textvariable=variable)
+) -> ttk.Combobox:
+    combobox = ttk.Combobox(parent, textvariable=variable)
     combobox["values"] = items
     combobox.grid(row=row, column=column, sticky=sticky)
     return combobox
 
 
 def create_entry(
-    parent: tk.Widget,
+    parent: ttk.Widget,
     text: str,
-    variable: StringVar,
+    variable: tk.StringVar,
     row: int,
     column: int,
     sticky: str = "",
-) -> tk.Entry:
-    entry = tk.Entry(parent, textvariable=variable)
+) -> ttk.Entry:
+    entry = ttk.Entry(parent, textvariable=variable)
     entry.insert(0, text)
     entry.grid(row=row, column=column, sticky=sticky)
     return entry
+
+
+def create_checkbox(
+    parent: ttk.Widget,
+    text: str,
+    variable: tk.BooleanVar,
+    row: int,
+    column: int,
+    sticky: str = "",
+) -> ttk.Checkbutton:
+    checkbox = ttk.Checkbutton(parent, text=text, variable=variable)
+    checkbox.grid(row=row, column=column, sticky=sticky)
+    return checkbox
