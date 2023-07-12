47f654908cd2ffe367bc96ffb2a3c61c3a32a977
koetsmax <koetsmax@gmail.com>
2023-07-12 22:28:36 +0200

    Added CreateSettingsWindow class in widgets

--- modules/submodules/functions/widgets.py
+++ modules/submodules/functions/widgets.py
@@ -5,6 +5,7 @@ This is a helper module for creating widgets.
 from tkinter import ttk
 import tkinter as tk
 from typing import List, Callable, Union
+import configparser
 
 
 def create_button(
@@ -90,3 +91,80 @@ def create_checkbox(
     checkbox = ttk.Checkbutton(parent, text=text, variable=variable)
     checkbox.grid(row=row, column=column, sticky=sticky)
     return checkbox
+
+
+class CreateSettingsWIndow:
+    """
+    Creates a window for settings.
+    """
+
+    def __init__(self, root: Union[tk.Toplevel, ttk.Frame], _config: List[str]):
+        self.config = configparser.ConfigParser()
+        self.config.read("settings.ini")
+
+        # extract config from list
+        window_title = _config[0]
+        explanation = _config[1]
+        text = _config[2]
+        self.settings = _config[3]
+        self.variables = _config[4]
+        self.defaults = _config[5]
+
+        settings_window = tk.Toplevel()
+        settings_window.title(window_title)
+
+        self.var1 = self.variables[0]
+        print(self.settings)
+        print(self.var1)
+
+        self.variable1 = tk.StringVar(value=self.config[self.settings][self.variables[0]])
+        print(self.variable1, self.variable1.get())
+        if self.variables[1] is not None:
+            self.variable2 = tk.StringVar(value=self.config[self.settings][self.variables[1]])
+
+        # Create the labels
+        create_label(settings_window, explanation, 1, 1, "W", 2)
+        create_label(settings_window, text[0], 3, 1, "W")
+        if text[1] is not None:
+            create_label(settings_window, text[1], 5, 1, "W")
+
+        # Create the entries
+        self.entry1 = create_entry(settings_window, self.variable1, 4, 1, "E, W")
+        if self.variables[1] is not None:
+            self.entry2 = create_entry(settings_window, self.variable2, 6, 1, "E, W")
+
+        # Create the buttons
+        create_button(settings_window, "Save Changes", lambda: self.save_changes(), 7, 1, "W")  # pylint: disable=W0108, line-too-long
+        create_button(settings_window, "Reset To Default", lambda: self.reset_to_default(), 7, 1, "E")  # pylint: disable=W0108, line-too-long
+
+        for child in settings_window.winfo_children():
+            child.grid_configure(padx=5, pady=5)
+
+        root.eval(f"tk::PlaceWindow {str(settings_window)} center")  # type: ignore
+
+    def save_changes(self):
+        """
+        Saves the changes made in the settings window.
+        """
+        with open("settings.ini", "w", encoding="UTF-8") as configfile:
+            try:
+                self.config[self.settings][self.variables[0]] = self.entry1.get()
+                if self.variables[1] is not None:
+                    self.config[self.settings][self.variables[1]] = self.entry2.get()
+            except AttributeError:
+                pass
+            self.config.write(configfile)
+
+    def reset_to_default(self):
+        """
+        Resets the settings to default.
+        """
+        with open("settings.ini", "w", encoding="UTF-8") as configfile:
+            self.variable1.set(self.defaults[0])
+            self.config[self.settings][self.variables[0]] = self.defaults[0]
+
+            if self.variables[1] is not None:
+                self.variable2.set(self.defaults[1])
+                self.config[self.settings][self.variables[1]] = self.defaults[1]
+
+            self.config.write(configfile)
