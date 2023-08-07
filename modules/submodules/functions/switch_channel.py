550ff710e34662506c473c87ed3deb97223f31a7
koetsmax <koetsmax@gmail.com>
2023-08-07 21:47:07 +0200

    removed self argument rem,oved unnecesary print statement

--- modules/submodules/functions/switch_channel.py
+++ modules/submodules/functions/switch_channel.py
@@ -6,11 +6,10 @@ import keyboard
 from .clear_typing_bar import clear_typing_bar
 
 
-def switch_channel(self, channel: str, *args):
+def switch_channel(channel: str, *args):
     """
     This module attempts to switch between discord channels
     """
-    print(channel)
     if not args:
         clear_typing_bar()
     keyboard.press_and_release("ctrl+k")
