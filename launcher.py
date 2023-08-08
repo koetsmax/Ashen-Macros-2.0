936dbc44168aa9f394f6a013f9a0248038f68381
koetsmax <koetsmax@gmail.com>
2023-08-08 16:19:16 +0200

    Fixed issue where you couldn't close check for updates window

--- launcher.py
+++ launcher.py
@@ -137,7 +137,7 @@ class Launcher:
         if update_is_available:
             widgets.create_button(updatewindow, "Yes", lambda: self.commence_update, 2, 1, "E")
         else:
-            widgets.create_button(updatewindow, "Okay", lambda: updatewindow.destroy, 2, 1, "W")
+            widgets.create_button(updatewindow, "Okay", lambda: updatewindow.destroy(), 2, 1, "W")
 
         for child in updatewindow.winfo_children():
             child.grid_configure(padx=5, pady=5)
