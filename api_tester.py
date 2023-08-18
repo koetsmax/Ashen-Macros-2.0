66af18f69d1d22079491c350b2b53f15fa91b162
koetsmax <koetsmax@gmail.com>
2023-08-18 22:15:22 +0200

    added api_tester to gitignore

--- api_tester.py
+++ api_tester.py
@@ -5,7 +5,7 @@ import os
 
 url = "http://127.0.0.1:8000/elemental"
 
-payload = {"userID": "347067894741336064", "gamertag": "FlipperParty741"}
+payload = {"userID": "1026423270985170994", "gamertag": "FlipperParty741"}
 
 # send post request
 r = requests.post(url=url, json=payload, timeout=30)
