a19ef7aaf617c48ba3102345848943140e32a367
koetsmax <koetsmax@gmail.com>
2023-09-30 15:46:58 +0200

    Added threading lock

--- modules/submodules/functions/switch_channel.py
+++ modules/submodules/functions/switch_channel.py
@@ -6,16 +6,17 @@ import keyboard
 from .clear_typing_bar import clear_typing_bar
 
 
-def switch_channel(channel: str, *args, **kwargs):
+def switch_channel(self, channel: str, *args, **kwargs):
     """
     This module attempts to switch between discord channels
     """
-    print(channel)
-    if not args:
-        clear_typing_bar()
-    keyboard.press_and_release("ctrl+k")
-    time.sleep(0.18)
-    keyboard.write(channel)
-    time.sleep(0.8 if not kwargs else 5)
-    keyboard.press_and_release("enter")
-    time.sleep(2)
+    with self.keyboard_lock:
+        print(channel)
+        if not args:
+            clear_typing_bar()
+        keyboard.press_and_release("ctrl+k")
+        time.sleep(0.18)
+        keyboard.write(channel)
+        time.sleep(0.8 if not kwargs else 5)
+        keyboard.press_and_release("enter")
+        time.sleep(2)
