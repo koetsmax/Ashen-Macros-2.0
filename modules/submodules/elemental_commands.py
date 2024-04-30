e14999c43c6ba08048a15cc84bfbbe15e41adc8c
Max Koets <koetsmax@gmail.com>
2024-04-30 12:46:44 +0200

    Refactor code for improved readability and maintainability

--- modules/submodules/elemental_commands.py
+++ modules/submodules/elemental_commands.py
@@ -34,13 +34,21 @@ def elemental_commands(self, *args):
     # self.notespage = 2
     if not args:
         # self.function_button.config(text="Add GT to Notes", command=lambda: add_note(self))
-        # self.kill_button.config(text=f"Check notes page {self.notespage}", command=lambda: check_notes_page(self))
-        # self.start_button.config(text="Continue", command=lambda: modules.submodules.start_check.continue_to_next(self))
+        # self.kill_button.config(
+        #     text=f"Check notes page {self.notespage}", command=lambda: check_notes_page(self)
+        # )
+        # self.start_button.config(
+        #     text="Continue", command=lambda: modules.submodules.start_check.continue_to_next(self)
+        # )
         # self.start_button.state(["!disabled"])
         modules.submodules.start_check.continue_to_next(self)
     else:
-        self.function_button.config(text="Tell to link xbox", command=lambda: tell_to_link_xbox(self))
-        self.kill_button.config(text="Tell to verify + link xbox", command=lambda: tell_to_verify_link_xbox(self))
+        self.function_button.config(
+            text="Tell to link xbox", command=lambda: tell_to_link_xbox(self)
+        )
+        self.kill_button.config(
+            text="Tell to verify + link xbox", command=lambda: tell_to_verify_link_xbox(self)
+        )
         self.start_button.state(["disabled"])
 
 
@@ -91,7 +99,9 @@ def tell_to_link_xbox(self):
     self.kill_button.state(["!disabled"])
     self.start_button.state(["!disabled"])
     self.currentstate = "SOTOfficial"
-    self.start_button.config(text="Continue", command=lambda: modules.submodules.start_check.continue_to_next(self))
+    self.start_button.config(
+        text="Continue", command=lambda: modules.submodules.start_check.continue_to_next(self)
+    )
 
 
 def tell_to_verify_link_xbox(self):
@@ -108,33 +118,51 @@ def tell_to_verify_link_xbox(self):
     self.kill_button.state(["!disabled"])
     self.start_button.state(["!disabled"])
     self.currentstate = "SOTOfficial"
-    self.start_button.config(text="Continue", command=lambda: modules.submodules.start_check.continue_to_next(self))
+    self.start_button.config(
+        text="Continue", command=lambda: modules.submodules.start_check.continue_to_next(self)
+    )
 
 
 def make_api_request(self):
+    """
+    This function makes the API request
+    """
     try:
         if self.method.get() == "All Commands":
             elemental_api_request(self)
 
-    except Exception as e:
+    except Exception as e:  # pylint: disable=broad-except
         print(f"API Request Error: {e}")
 
 
 # Create a function to start API requests in a separate thread
 def start_elemental_api_requests_thread(self):
+    """
+    This function starts the Elemental API requests in a separate thread.
+    """
     api_thread = threading.Thread(target=make_api_request, args=(self,))
     api_thread.start()
 
 
 def elemental_api_request(self):
+    """
+    This function sends an API request to the Elemental API.
+    """
     request_error = False
     if self.channel.get() == "#on-duty-commands":
         self.loghistory_status_label.config(text="Sending API request", foreground="orange")
         self.mainframe.update()
         try:
-            #! still perform the request even if the user has no gamertag, so we can run the loghistory API
-            payload = {"userID": self.user_id.get(), "gamertag": self.xbox_gt if self.xbox_gt else "abcdefghij", "timestamp": self.timestamp}
-            response = requests.post(f"{self.api_url}/elemental", json=payload, verify=False)
+            #! Still perform the request even if the user has no gamertag.casefold
+            #! So we can run the loghistory API
+            payload = {
+                "userID": self.user_id.get(),
+                "gamertag": self.xbox_gt if self.xbox_gt else "abcdefghij",
+                "timestamp": self.timestamp,
+            }
+            response = requests.post(
+                f"{self.api_url}/elemental", json=payload, verify=False, timeout=20
+            )
 
             if response.status_code != 200:
                 request_error = True
@@ -142,14 +170,34 @@ def elemental_api_request(self):
                 self.loghistory_status_label.config(text="Command not found!", foreground="red")
             else:
                 response_json = response.json()
-                self.account_age_label.config(text=f"{response_json['account_age']} Days", foreground="red" if response_json["account_age"] < 60 else "green")
-                self.needs_warning_talk_label.config(text=f"{response_json['needs_warning_talk']}", foreground="red" if response_json["needs_warning_talk"] else "green")
-                self.gamertag_in_notes_label.config(text=f"{response_json['gamertag_in_notes']}", foreground="green" if response_json["gamertag_in_notes"] else "red")
-                self.needs_to_be_spoken_to_label.config(text=f"{response_json['needs_to_be_spoken_to']}", foreground="red" if response_json["needs_to_be_spoken_to"] else "green")
-                self.needs_mic_check_label.config(text=f"{response_json['needs_mic_check']}", foreground="red" if response_json["needs_mic_check"] else "green")
-                self.anti_alliance_note_label.config(text=f"{response_json['anti_alliance_note']}", foreground="red" if response_json["anti_alliance_note"] else "green")
+                self.account_age_label.config(
+                    text=f"{response_json['account_age']} Days",
+                    foreground="red" if response_json["account_age"] < 60 else "green",
+                )
+                self.needs_warning_talk_label.config(
+                    text=f"{response_json['needs_warning_talk']}",
+                    foreground="red" if response_json["needs_warning_talk"] else "green",
+                )
+                self.gamertag_in_notes_label.config(
+                    text=f"{response_json['gamertag_in_notes']}",
+                    foreground="green" if response_json["gamertag_in_notes"] else "red",
+                )
+                self.needs_to_be_spoken_to_label.config(
+                    text=f"{response_json['needs_to_be_spoken_to']}",
+                    foreground="red" if response_json["needs_to_be_spoken_to"] else "green",
+                )
+                self.needs_mic_check_label.config(
+                    text=f"{response_json['needs_mic_check']}",
+                    foreground="red" if response_json["needs_mic_check"] else "green",
+                )
+                self.anti_alliance_note_label.config(
+                    text=f"{response_json['anti_alliance_note']}",
+                    foreground="red" if response_json["anti_alliance_note"] else "green",
+                )
                 self.jump_to_message_button.state(["!disabled"])
-                self.jump_to_message_button.config(command=lambda: switch_channel(self, response_json["jump_url"], kwargs=True))
+                self.jump_to_message_button.config(
+                    command=lambda: switch_channel(self, response_json["jump_url"], kwargs=True)
+                )
 
                 issues = {
                     "Account Age": response_json["account_age"] < 60,
@@ -162,7 +210,10 @@ def elemental_api_request(self):
 
                 # Add the issues to the list
                 self.loghistory_issues = [issue for issue, has_issue in issues.items() if has_issue]
-                self.loghistory_status_label.config(text=f"{len(self.loghistory_issues)} issue(s) found", foreground="red" if self.loghistory_issues else "green")
+                self.loghistory_status_label.config(
+                    text=f"{len(self.loghistory_issues)} issue(s) found",
+                    foreground="red" if self.loghistory_issues else "green",
+                )
                 if self.loghistory_issues:
                     self.loghistory_fix_issues_button.state(["!disabled"])
 
@@ -176,11 +227,17 @@ def elemental_api_request(self):
 
 
 def fix_issues(self):
+    """
+    This function fixes all issues found in the loghistory API response.
+    """
     if "Gamertag in Notes" in self.loghistory_issues:
         add_note(self)
         self.loghistory_issues.remove("Gamertag in Notes")
         self.gamertag_in_notes_label.config(text="True", foreground="green")
 
-    self.loghistory_status_label.config(text=f"{len(self.loghistory_issues)} issue(s) found", foreground="red" if self.loghistory_issues else "green")
+    self.loghistory_status_label.config(
+        text=f"{len(self.loghistory_issues)} issue(s) found",
+        foreground="red" if self.loghistory_issues else "green",
+    )
     if not self.loghistory_issues:
         self.loghistory_fix_issues_button.state(["disabled"])
