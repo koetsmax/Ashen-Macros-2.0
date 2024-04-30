f433d448a6bf1519d6570fc65a9dae14ee358777
koetsmax <koetsmax@gmail.com>
2024-04-30 19:53:21 +0200

    refactor: Update queue monitor title and add option to disable tear-off

--- modules/queue.py
+++ modules/queue.py
@@ -13,7 +13,8 @@ class Queue:
         self.root = root
         self.sio = sio or ThreadedSocketClient(url="http://localhost:5000", auth="Controller")
 
-        self.root.title("Queue")
+        self.root.title("Queue Monitor")
+        self.root.option_add("*tearOff", FALSE)
 
         @self.sio.event()
         def connect():
@@ -25,6 +26,10 @@ class Queue:
 
         @self.sio.event()
         def queue(data):
+            print(data)
+
+        @self.sio.event()
+        def info(data):
             print(data)
 
 
@@ -49,3 +54,24 @@ def start_script():
         root.mainloop()
     except Exception:
         traceback.print_exc()
+
+
+# on duty staff
+# number of fleets
+# number of ships
+# any unidentifiable ships
+# number of ships that need people
+
+# number of people in queue normal
+# number of people in queue per activity
+# number of people shipswap vs normal
+# any unidentifiable queue requests
+
+
+# Need to spike another fleet?
+# Spike difficulty (time of day, people in queue, day of week)
+# Discord API status (use ping command from bot or scrape discordstatus.com)
+
+# Pings in fleet chat without reaction (account for no read access)
+# Pings in bot notices without reaction (account for no read access)
+# New pings in last 15 seconds in queue
