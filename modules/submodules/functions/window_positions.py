511c6bcbbaf4537c70e1ee0465974b64b65f83a8
Max Koets <koetsmax@gmail.com>
2024-04-30 13:02:16 +0200

    Refactor window_positions module for improved readability and maintainability

--- modules/submodules/functions/window_positions.py
+++ modules/submodules/functions/window_positions.py
@@ -1,13 +1,27 @@
+"""
+This module is responsible for saving and loading the window position
+"""
+
 import configparser
 import os
 
 
 def save_window_position(window, *args):
+    """
+    Saves the window position to a file.
+    """
     config = configparser.ConfigParser()
     config.read(os.path.expanduser("~/Documents/Ashen Macros/settings.ini"))
-    config["WINDOW"] = {"x_offset": str(window.winfo_x()), "y_offset": str(window.winfo_y())}
+    config["WINDOW"] = {
+        "x_offset": str(window.winfo_x()),
+        "y_offset": str(window.winfo_y()),
+    }
     try:
-        with open(os.path.expanduser("~/Documents/Ashen Macros/settings.ini"), "w", encoding="UTF-8") as file:
+        with open(
+            os.path.expanduser("~/Documents/Ashen Macros/settings.ini"),
+            "w",
+            encoding="UTF-8",
+        ) as file:
             config.write(file)
     except PermissionError as e:
         print("PermissionError: Could not save window position.\n", e)
@@ -17,7 +31,12 @@ def save_window_position(window, *args):
 
 
 def load_window_position(window):
+    """
+    Loads the window position from a file.
+    """
     config = configparser.ConfigParser()
     config.read(os.path.expanduser("~/Documents/Ashen Macros/settings.ini"))
     if "WINDOW" in config:
-        window.geometry(f"+{config['WINDOW']['x_offset']}+{config['WINDOW']['y_offset']}")
+        window.geometry(
+            f"+{config['WINDOW']['x_offset']}+{config['WINDOW']['y_offset']}"
+        )
