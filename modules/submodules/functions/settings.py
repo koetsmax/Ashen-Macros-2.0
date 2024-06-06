ae1493422c991854ac8692eff5e8942799ded709
koetsmax <koetsmax@gmail.com>
2024-06-06 21:29:24 +0200

    Made config file more robust by using a lock and resetting the file if errors occured

--- modules/submodules/functions/settings.py
+++ modules/submodules/functions/settings.py
@@ -1,5 +1,10 @@
 import configparser
 import os
+from filelock import FileLock, Timeout
+
+CONFIG_FILE_PATH = os.path.expanduser("~/Documents/Ashen Macros/settings.ini")
+LOCK_FILE_PATH = CONFIG_FILE_PATH + ".lock"
+LOCK_TIMEOUT = 30  # Timeout duration in seconds
 
 
 def read_config() -> dict:
@@ -7,10 +12,14 @@ def read_config() -> dict:
     Read the config file.
     """
     config = configparser.ConfigParser()
-    _read_config_file(config)
-    _set_default_values(config)
+    with FileLock(LOCK_FILE_PATH, timeout=LOCK_TIMEOUT):
+        try:
+            _read_config_file(config)
+        except (configparser.Error, FileNotFoundError):
+            _set_default_values(config)
+            _write_config_file(config)
 
-    return _read_config_values(config)
+        return _read_config_values(config)
 
 
 def set_custom_value(section, option, value):
@@ -18,29 +27,27 @@ def set_custom_value(section, option, value):
     Set a custom value in the config.
     """
     config = configparser.ConfigParser()
-    config_file_path = os.path.expanduser("~/Documents/Ashen Macros/settings.ini")
-    config.read(config_file_path)
+    with FileLock(LOCK_FILE_PATH, timeout=LOCK_TIMEOUT):
+        try:
+            config.read(CONFIG_FILE_PATH)
+        except (configparser.Error, FileNotFoundError):
+            _set_default_values(config)
 
-    if section not in config:
-        config[section] = {}
-    config[section][option] = value
-
-    _write_config_file(config)
+        if section not in config:
+            config[section] = {}
+        config[section][option] = value
+        _write_config_file(config)
 
 
 def _read_config_file(config) -> None:
-    config_file_path = os.path.expanduser("~/Documents/Ashen Macros/settings.ini")
-    config.read(config_file_path)
-    _read_config_values(config)
+    config.read(CONFIG_FILE_PATH)
 
 
 def _read_config_values(config) -> dict:
     settings = {}
-
     for section in config.sections():
         for option, value in config.items(section):
             settings[option] = value
-
     return settings
 
 
@@ -67,10 +74,7 @@ def _set_default_values(config):
                 if option not in config[section]:
                     config[section][option] = value
 
-    _write_config_file(config)
-
 
 def _write_config_file(config):
-    config_file_path = os.path.expanduser("~/Documents/Ashen Macros/settings.ini")
-    with open(config_file_path, "w", encoding="UTF-8") as configfile:
+    with open(CONFIG_FILE_PATH, "w", encoding="UTF-8") as configfile:
         config.write(configfile)
