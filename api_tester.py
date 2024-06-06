80d6abcf133489ac756335e7c7ff474dd369226b
koetsmax <koetsmax@gmail.com>
2024-06-06 22:37:48 +0200

    Added api_tester

---
+++ api_tester.py
@@ -0,0 +1,64 @@
+import requests
+import json
+
+LOCAL_URL = "https://localhost:8000"
+TESTING_URL = "https://192.168.1.2:8000"
+PRODUCTION_URL = "https://ashen_api.famkoets.nl"
+
+# print all 3 of the urls and let the user pick a number one to 3 for which one to use
+print("1) Localhost (https://localhost:8000)")
+print("2) Testing (ttps://192.168.1.2:8000)")
+print("3) Production (https://ashen_api.famkoets.nl)")
+url = input("Enter the number of the URL you want to use: ")
+
+if url == "1":
+    URL = LOCAL_URL
+elif url == "2":
+    URL = TESTING_URL
+elif url == "3":
+    URL = PRODUCTION_URL
+
+payload = {"userID": "287048710519783426", "gamertag": "R1chardb68"}
+payload = {"test": "true"}
+# test the connection GET endpoint
+
+endpoints = {
+    "connection": "get",
+    "validate_token": "post",
+    "staffcheck": "post",
+    "elemental": "post",
+    "search": "post",
+    "sotofficial": "post",
+    "invite": "post",
+}
+
+results = []
+
+for endpoint, method in endpoints.items():
+    if method == "get":
+        response = requests.get(url=URL + "/" + endpoint, timeout=30, verify=False)
+    elif method == "post":
+        response = requests.post(
+            url=URL + "/" + endpoint, json=payload, timeout=30, verify=False
+        )
+    try:
+        if response.json()["test"] == "success":
+            results.append(f"{endpoint} is working")
+        else:
+            results.append(f"{endpoint} is not working")
+    except Exception:  # pylint: disable=broad-except
+        if response.status_code == 200 and method == "get":
+            results.append(f"{endpoint} is working")
+        else:
+            results.append(f"{endpoint} is not working")
+
+print("\n\n\n~~~~~~~~~~~~~~~RESULTS~~~~~~~~~~~~~~~\n")
+
+# make the text green if it is working and red if it is not
+for result in results:
+    if "working" in result:
+        print(f"\033[92m{result}\033[0m")
+    else:
+        print(f"\033[91m{result}\033[0m")
+
+input("\n\nPress Enter to exit")
